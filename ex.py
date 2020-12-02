7edb2c94c2c20c3091921fc88940031a6f54146c
912152-asma <58762111+912152-asma@users.noreply.github.com>
2020-12-02 14:22:01 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -2,3 +2,4 @@ print("""Python is very popular language.
 				asma is very good girl in the world
 				Python is an easy to learn, powerful programming language. It has efficient high-level data structures and a simple but effective approach to object-oriented programming. Python’s elegant syntax and dynamic typing, together with its interpreted nature, make it an ideal language for scripting and rapid application development in many areas on most platforms.
 It is also friendly language.""")
+print("hello")
